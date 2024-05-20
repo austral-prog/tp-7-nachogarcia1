fa46d7c74b128a06ced00e7ef800f18fd234e9d8
nachogarcia1 <igarcia1@mail.austral.edu.ar>
2024-05-20 18:45:46 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,23 @@
-def enumerate_list(list):
-    return list
+
+def enumerate_list(colors):
+    newlist = []
+    index = 0
+    for element in colors:
+        if len(element) > 0:
+            newlist.append(f"{index}. {element}")
+            index += 1
+    return newlist
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(colors):
+    newlist2 = []
+    index2 = 0
+    for element in colors:
+        if len(element) > 0:
+            newlist2.append(f"{index2}. {element [::-1]}")
+            index2 += 1
+    return newlist2
+
+
+print(enumerate_list(colors))
+print(enumerate_backwards(colors))

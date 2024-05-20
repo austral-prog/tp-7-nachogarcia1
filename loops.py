66006a443970fcc9f2b240a8283ae3ea5e390ef6
nachogarcia1 <igarcia1@mail.austral.edu.ar>
2024-05-20 18:45:20 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,37 @@
 def index_of_by_index(word, list, index):
+    for index2, element in enumerate(list[index:]):
+        if element == word:
+            return index2 + index
     return -1
 
 
 def index_of_empty(list):
+    for index, element in enumerate(list):
+        if element == "":
+            return index
     return -1
 
 
 def index_of(word, list):
+    for index, element in enumerate(list):
+        if element == word:
+            return index
     return -1
 
 
 def put(word, list):
+    for index, element in enumerate(list):
+        if element == "":
+            list[index] = word
+            return index
     return -1
 
 
 def remove(word, list):
+    count = 0
+    for index, element in enumerate(list):
+        if element == word:
+            list[index] = ""
+            count += 1
+    return count
     return -1
